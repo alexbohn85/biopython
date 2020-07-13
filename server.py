a80796a45737332042731af711cf104613e45d4c
Alex Bohn <alexbohn85@gmail.com>
2020-07-13 01:50:38 -0700

    new

--- server.py
+++ server.py
@@ -1,24 +1,17 @@
-#!/usr/bin/env python
-# Import smtplib for the actual sending function
-import smtplib
+import requests
+import sched, time
 
-# Import the email modules we'll need
-from email.mime.text import MIMEText
+url = 'https://www.cic.gc.ca/english/information/where-to-give-biometrics.asp'
+s = sched.scheduler(time.time, time.sleep)
 
-# Open a plain text file for reading.  For this example, assume that
-# the text file contains only ASCII characters.
-# Create a text/plain message
-msg = MIMEText("Teste Teste Teste!!")
+loc_response = requests.get(url)
 
+def check_changes(url):
+    response = requests.get(url)
+    if loc_response == response:
+        print('They are the same')
+    else
+        print('They are different')
 
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'The contents of %s' % textfile
-msg['From'] = "teste@teste.com"
-msg['To'] = "teste@teste.com"
-
-# Send the message via our own SMTP server, but don't include the
-# envelope header.
-s = smtplib.SMTP('localhost')
-s.sendmail("teste@teste.com", "teste@teste.com", msg.as_string())
-s.quit()
+s.enter(4, 1, check_changes, url)
+s.run()
